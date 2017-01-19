8581d35b1012f8896327e384026000b42424967e
Alexandre Devienne <grypoB@gmail.com>
2017-01-19 14:42:42 -0500

    Added cmd 5 for detection

--- ObstacleDetection.ino
+++ ObstacleDetection.ino
@@ -35,44 +35,24 @@ void command1() {
   
   //Serial.println("done");
 }
+
+void command5() {
+  Serial.println("cmd 5");
+  ultra.burstAndCapture(LONG);
+  
+  uint16_t high = ultra.serial.read();
+  uint16_t low = ultra.serial.read();
+  uint16_t timeOfFlight = ((high<<8) + low);
+  Serial.print(timeOfFlight, HEX);
+  Serial.print("\t");
+  double distance = ((double) timeOfFlight) * SPEED/2000;
+  Serial.println(distance);
+  uint8_t checksum = ultra.serial.read();
+  
+  Serial.println("cmd 5 end");
+}
 void loop() {
-  /*
-  // Tell board to do a long pulse
-  Serial.println("Sending sensing command to board");
-  mySerial.write((uint8_t)0x00);
-  mySerial.write((uint8_t)0x55);
-  mySerial.write((uint8_t)0x51);
-  mySerial.write((uint8_t)0x02);
-  mySerial.write((uint8_t)0x00);
-  delay(1000);
-
-  //Read the FIFO memory
-  Serial.println("Sending read command");
-  mySerial.write((uint8_t)0x00);
-  mySerial.write((uint8_t)0x55);
-  mySerial.write((uint8_t)0x41);
-  mySerial.write((uint8_t)0x00);
-
-  uint8_t i = 0;
-  while(mySerial.available()) {
-    Serial.print("Current count: ");
-    Serial.print(i);
-    Serial.print(" Byte: ");
-    Serial.println(mySerial.read(), HEX);
-    ++i;
-  }
-  delay(5000);
-  while(mySerial.available()) {
-    Serial.print("Current count: ");
-    Serial.print(i);
-    Serial.print(" Byte: ");
-    Serial.println(mySerial.read(), HEX);
-    ++i;
-  }
-  Serial.println("\nEnd of message");
-  delay(10000);
-  */
   //testUART();
-  command1();
+  command5();
   delay(100);
 }
