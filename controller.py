c0955d77baaf1ecad5ca89c70d76e2f4795d838a
Sean Buckley <sbuckley12489@gmail.com>
2017-01-19 21:18:35 -0500

    Adding PID (without integral nor derivative) controller

--- controller.py
+++ controller.py
@@ -7,6 +7,9 @@ import struct
 uno = serial.Serial('/dev/cu.usbmodem14221',57600)
 mega= serial.Serial('/dev/tty.usbmodem14241',76800)
 
+GOAL = 150
+P = 1
+
 def getDist():
     line = uno.readline()
     dist = 0
@@ -24,28 +27,38 @@ def sendMega(message_id,message_data):
     mega.write(message_buffer)
     mega.flush()
 
-def breakCmd(value):
+def brakeCmd(value):
     sendMega(18,value)
 
 def steerCmd(value):
     sendMega(19,value)
 
 def main():
-    breakValue = 0
-    dists = [1e4,1e4,1e4]
+    brakeValue = 0
+    #dists = [1e4,1e4,1e4]
     pDist = 0
     threshold = 1000
     steerValue = -130
     while True:
         dist = getDist()
-        dists[pDist] = dist
-        pDist = (pDist+1)%3
-        average = sum(dists)/len(dists)
-        print average, breakValue
-        if dist<threshold:
-            breakValue = 1
-        breakCmd(breakValue)
-        steerCmd(steerValue)
+
+        #dists[pDist] = dist
+        #pDist = (pDist+1)%3
+        #average = sum(dists)/len(dists)
+        #print average, brakeValue
+
+        #dist = distance to obstacle in centimeters.  We're looking to keep it around 150
+
+        error = int(GOAL - dist)
+
+        steerCmd(P * error)
+
+
+        print dist
+
+        #if dist<threshold:
+        #    brakeValue = 1
+        brakeCmd(brakeValue)
 
 
 if __name__ == "__main__":
