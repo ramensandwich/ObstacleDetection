37192f38bffbe0fdef21d2675cb1e76a74ef7d1b
Alexandre Devienne <grypoB@gmail.com>
2017-01-19 20:54:49 -0500

    Optimised cmd5 timing (20Hz)

--- ObstacleDetection.ino
+++ ObstacleDetection.ino
@@ -37,22 +37,22 @@ void command1() {
 }
 
 void command5() {
-  Serial.println("cmd 5");
+  //Serial.println("cmd 5");
   ultra.burstAndCapture(LONG);
   
   uint16_t high = ultra.serial.read();
   uint16_t low = ultra.serial.read();
   uint16_t timeOfFlight = ((high<<8) + low);
-  Serial.print(timeOfFlight, HEX);
-  Serial.print("\t");
+  //Serial.print(timeOfFlight, HEX);
+  //Serial.print("\t");
   double distance = ((double) timeOfFlight) * SPEED/2000;
   Serial.println(distance);
   uint8_t checksum = ultra.serial.read();
   
-  Serial.println("cmd 5 end");
+  //Serial.println("cmd 5 end");
 }
 void loop() {
   //testUART();
   command5();
-  delay(100);
+  delay(50);
 }
